80a5d06ce46b691d654ee894808a5cfa4e600c0a
Matthew Cranford <mcranfordmail@gmail.com>
2018-05-19 18:47:16 -0700

    Add display game board

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ let gameBoard = [
   [0, 2, 1], 
   [2, 0, 1]
 ];
+
+function displayGameBoard() {
+  let rowId = 0;
+  let squareId = 1;
+
+  for (let row of gameBoard) {
+    squareId = 0;
+    for (let square of row) {
+      if (square === 1) {
+        $(`#row${rowId} div:nth-child(${squareId + 1})`).text('X');
+      } else if (square === 2) {
+        $(`#row${rowId} div:nth-child(${squareId + 1})`).text('O');
+      }
+      squareId++;
+    }
+    rowId++;
+  }
+}
+
+displayGameBoard();
